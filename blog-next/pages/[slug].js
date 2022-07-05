3b43e7e8c39d66cca6f08ecd53ac8d6b5774fe99
derekhongg <hongderek@gmail.com>
2022-07-04 19:28:49 -0700

    fix rendering issues

--- blog-next/pages/[slug].js
+++ blog-next/pages/[slug].js
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Post({ post }) {
     return (
         <div>
-            <div>{post.Title}</div>
+            <div>{post.attributes.Title}</div>
         </div>
     )
 };
@@ -13,8 +13,8 @@ export async function getStaticPaths() {
     const res = await fetch(`http://localhost:1337/api/posts/`);
     const posts = await res.json();
     console.log(posts);
-    
-    const paths = posts.map((post) => ({
+
+    const paths = posts.data.map((post) => ({
         params: { slug: post.attributes.Slug },
     }));
 
@@ -30,11 +30,10 @@ export async function getStaticPaths() {
 //for reach individual page: get data for that page
 export async function getStaticProps({ params }) {
     const { slug } = params;
-
     const res = await fetch(`http://localhost:1337/api/posts?filters[slug][$eq]=${slug}`);
     const data = await res.json();
-    const post = data[0];
-
+    const post = data.data[0];
+    
     return {
         props: { post },
     };
